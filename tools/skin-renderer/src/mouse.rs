e6210001dde71b69c93db4fb7d5fc4e21c8e54d8
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-11-17 23:48:23 +0000

    Clean-up mouse stuff

--- tools/skin-renderer/src/mouse.rs
+++ tools/skin-renderer/src/mouse.rs
@@ -22,53 +22,49 @@ pub fn rotate_camera(scene: &mut Scene, ctx: &GraphicsContext, yaw: f32, pitch: 
     rotation.pitch = pitch;
     rotation.roll = roll;
 
-    let new_sun = {
-        let one_eighty_diff = (rotation.yaw.abs() - 180.0).abs();
-        let yaw = if one_eighty_diff < 0.01 {
-            camera.get_yaw().abs() + 90.0
-        } else if camera.get_yaw().is_sign_positive() || camera.get_yaw() <= -90.0 {
-            camera.get_yaw()
-        } else {
-            camera.get_yaw() + 90.0
-        };
-
-        let aligned_yaw = ((yaw + 180.0) / 90.0).floor() * 90.0;
-
-        let rot_quat: Quat = Quat::from_euler(
-            EulerRot::ZXY,
-            camera.get_roll().to_radians(),
-            0.0,
-            aligned_yaw.to_radians(),
-        );
-
-        let light = Vec3::new(0.0, -6.21, 6.21);
-        let front_lighting = rot_quat.mul_vec3(light) * Vec3::new(1.0, 1.0, -1.0);
-
-        scene.sun_information_mut().direction = front_lighting;
+    let one_eighty_diff = (rotation.yaw.abs() - 180.0).abs();
+    let yaw = if one_eighty_diff < 0.01 {
+        camera.get_yaw().abs() + 90.0
+    } else if camera.get_yaw().is_sign_positive() || camera.get_yaw() <= -90.0 {
+        camera.get_yaw()
+    } else {
+        camera.get_yaw() + 90.0
     };
+
+    let aligned_yaw = ((yaw + 180.0) / 90.0).floor() * 90.0;
+
+    let rot_quat: Quat = Quat::from_euler(
+        EulerRot::ZXY,
+        camera.get_roll().to_radians(),
+        0.0,
+        aligned_yaw.to_radians(),
+    );
+
+    let light = Vec3::new(0.0, -6.21, 6.21);
+    let front_lighting = rot_quat.mul_vec3(light) * Vec3::new(1.0, 1.0, -1.0);
+
+    scene.sun_information_mut().direction = front_lighting;
 
     scene.update(ctx);
 }
 
 pub fn handle_mouse_move(scene: &mut Scene, ctx: &GraphicsContext, x: f32, y: f32) {
     if unsafe { MOUSE_DOWN } {
-        if let Some(last_x) = unsafe { LAST_X } {
-            if let Some(last_y) = unsafe { LAST_Y } {
-                let x = x - last_x;
-                let y = y - last_y;
+        if let (Some(last_x), Some(last_y)) = unsafe { (LAST_X, LAST_Y) } {
+            let x = x - last_x;
+            let y = y - last_y;
 
-                let camera = scene.camera_mut();
-                let CameraRotation {
-                    mut yaw,
-                    mut pitch,
-                    mut roll,
-                } = &camera.get_rotation();
+            let camera = scene.camera_mut();
+            let CameraRotation {
+                mut yaw,
+                mut pitch,
+                roll,
+            } = &camera.get_rotation();
 
-                yaw += x;
-                pitch += y;
+            yaw += x;
+            pitch += y;
 
-                rotate_camera(scene, ctx, yaw, pitch, roll);
-            }
+            rotate_camera(scene, ctx, yaw, pitch, *roll);
         }
     }
 
